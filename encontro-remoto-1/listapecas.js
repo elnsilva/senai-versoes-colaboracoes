d50c0a41f8c9321b5379324c8b4bcbfbd95e4136
Ellen Silva <99916278+elnsilva@users.noreply.github.com>
2022-08-28 11:42:34 -0300

    Added listapecas.js
    Sistema de cadastro de peças - aula Senai.

---
+++ encontro-remoto-1/listapecas.js
@@ -0,0 +1,33 @@
+// Sistema de cadastro de peças Savinis.   
+// Requisitos:
+// * Peça com peso superior à 100 gramas;
+// * Quantidade de peças inferior à 10;
+// * Se nome da peça tiver menos que três caracteres, informar mensagem de erro.
+
+const listaDePecas = ['Selim','Aro','Quadro']
+
+console.log("------------------------")
+console.log("Início do cadastro de peças.")
+console.log("------------------------")
+
+let pesoPeca = 150
+if (pesoPeca > 100) {console.log("Próximo.")}
+else {console.log("Cadastro não permitido. Peça com peso inferior à 100 gramas.")}
+
+console.log("------------------------")
+
+let quantidadePeca = 10
+if (quantidadePeca > 10) {console.log("Quantidade de peças superior à capacidade da caixa.")}
+else {console.log("Próximo.")}
+
+console.log("------------------------")
+
+for (let index = 0; index < listaDePecas.length; index++) {
+        const nomePeca = listaDePecas[index];
+             if (nomePeca.length < 3) {console.log("Insira um nome de peça com três caracteres ou mais.")}
+             else {console.log("O nome da peça foi aceito.")}
+}
+
+console.log("------------------------")
+console.log("Fim do cadastro.")
+console.log("------------------------")
